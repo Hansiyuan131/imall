12148eeec4f1a551f0d3a5f2f891d741766a4566
Hansiyuan131 <422295068@qq.com>
2019-11-17 21:33:33 +0800

    搭建微服务网关

---
+++ imall-gateway/src/test/java/com/imall/gateway/ImallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
