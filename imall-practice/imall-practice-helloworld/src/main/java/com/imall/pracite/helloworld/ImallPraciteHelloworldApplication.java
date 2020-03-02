617e074abd9b37c87c27f45427789f311be659c7
Hansiyuan131 <422295068@qq.com>
2020-03-02 14:59:00 +0800

    add dev-plan and practice demo

---
+++ imall-practice/imall-practice-helloworld/src/main/java/com/imall/pracite/helloworld/ImallPraciteHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.imall.pracite.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImallPraciteHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImallPraciteHelloworldApplication.class, args);
+    }
+
+}
